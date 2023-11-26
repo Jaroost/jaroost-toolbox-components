73aff0cea0615eae8aba9b2a49d3d46a2b3cc80e
Antoine Jaquet <antoinejaquet@gmail.com>
2023-11-26 17:00:58 +0100

    svg creation in tools

--- src/element-tools.ts
+++ src/element-tools.ts
@@ -2,3 +2,8 @@ export function createElement(element:string):Element{
     let newElement=document.createElement(element)
     return newElement;
 }
+
+export function createSvg(svg:string): Element{
+    let parser = new DOMParser();
+    return parser.parseFromString(svg, "image/svg+xml").documentElement;
+}
